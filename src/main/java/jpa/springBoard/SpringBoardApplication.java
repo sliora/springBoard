151560efe87a771fb042887f010c645348113adb
zerom-H <cupdisin@gmail.com>
2022-09-16 21:57:55 +0900

    Initial commit

---
+++ src/main/java/jpa/springBoard/SpringBoardApplication.java
@@ -0,0 +1,13 @@
+package jpa.springBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoardApplication.class, args);
+	}
+
+}
