fc099ae7487e4109ceb2b9527e6dbe645be58c58
zerom-h <cupdisin@gmail.com>
2022-09-21 15:28:10 +0900

    Fix: #6 board 개발 CRUD 개발

--- src/main/java/jpa/springBoard/domain/board/repository/BoardRepositoryCustom.java
+++ src/main/java/jpa/springBoard/domain/board/repository/BoardRepositoryCustom.java
@@ -3,9 +3,11 @@ package jpa.springBoard.domain.board.repository;
 import jpa.springBoard.domain.board.dto.BoardRequest;
 import jpa.springBoard.domain.board.dto.BoardResponse;
 import jpa.springBoard.domain.board.dto.BoardSearchCondition;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface BoardRepositoryCustom {
-    List<BoardResponse> search(BoardSearchCondition condition);
+    Page<BoardResponse> search(BoardSearchCondition condition, Pageable pageable);
 }
--- src/main/java/jpa/springBoard/domain/board/repository/BoardRepositoryImpl.java
+++ src/main/java/jpa/springBoard/domain/board/repository/BoardRepositoryImpl.java
@@ -1,10 +1,15 @@
 package jpa.springBoard.domain.board.repository;
 
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpa.springBoard.domain.board.QBoard;
 import jpa.springBoard.domain.board.dto.BoardResponse;
 import jpa.springBoard.domain.board.dto.BoardSearchCondition;
+import jpa.springBoard.domain.board.dto.QBoardResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 
 import javax.persistence.EntityManager;
 import java.util.Arrays;
@@ -12,7 +17,6 @@ import java.util.List;
 
 import static jpa.springBoard.domain.board.QBoard.*;
 
-@RequiredArgsConstructor
 public class BoardRepositoryImpl implements BoardRepositoryCustom{
     private final JPAQueryFactory queryFactory;
 
@@ -21,7 +25,23 @@ public class BoardRepositoryImpl implements BoardRepositoryCustom{
     }
 
     @Override
-    public List<BoardResponse> search(BoardSearchCondition condition) {
-        return null;
+    public Page<BoardResponse> search(BoardSearchCondition condition, Pageable pageable) {
+        List<BoardResponse> result = queryFactory
+                .select(new QBoardResponse(
+                        board.id,
+                        board.title,
+                        board.content,
+                        board.deleted))
+                .from(board)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        JPAQuery<Long> countQuery = queryFactory
+                .select(board.count())
+                .from(board);
+
+        return PageableExecutionUtils.getPage(result, pageable, countQuery::fetchOne);
+
     }
 }
