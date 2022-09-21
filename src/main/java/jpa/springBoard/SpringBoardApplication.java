56d691262c585621b4f425db07597b46aa48594a
zerom-h <cupdisin@gmail.com>
2022-09-21 15:22:12 +0900

    Feat: #5 프로젝트 기본설정 스웨거, 베이스 엔티티 설정

--- src/main/java/jpa/springBoard/SpringBoardApplication.java
+++ src/main/java/jpa/springBoard/SpringBoardApplication.java
@@ -2,12 +2,26 @@ package jpa.springBoard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringBoardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoardApplication.class, args);
+	}
+
+
+	//임시로 작성자를 UUID로 설정한다.
+	@Bean
+	public AuditorAware<String> auditorProvider(){
+		return ()-> Optional.of(UUID.randomUUID().toString());
 	}
 
 }
---
+++ src/main/java/jpa/springBoard/global/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package jpa.springBoard.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                // 현재 RequestMapping으로 할당된 모든 URL 리스트를 추출
+                .paths(PathSelectors.ant("/api/**")) // 그중 /api/** 인 URL들만 필터링
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("springBoard")
+                .version("v1.0")
+                .description("springBoard")
+                .build();
+    }
+}
