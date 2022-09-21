ac228f779b97e564bb912c2a6c6e55304eed4fb2
zerom-h <cupdisin@gmail.com>
2022-09-21 15:28:10 +0900

    Fix: #6 board 개발 CRUD 개발

--- src/main/java/jpa/springBoard/domain/board/BoardService.java
+++ src/main/java/jpa/springBoard/domain/board/BoardService.java
@@ -1,8 +1,15 @@
 package jpa.springBoard.domain.board;
 
+import jpa.springBoard.domain.board.dto.BoardResponse;
+import jpa.springBoard.domain.board.dto.BoardSearchCondition;
 import jpa.springBoard.domain.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,8 +17,43 @@ public class BoardService {
 
     private final BoardRepository boardRepository;
 
-    public void saveBoard(Board board) {
-        boardRepository.save(board);
+    @Transactional
+    public Long saveBoard(Board request) {
+
+        return boardRepository.save(request).getId();
     }
+
+    public Page<BoardResponse> getBoardList(BoardSearchCondition condition, Pageable pageable) {
+        return boardRepository.search(condition, pageable);
+    }
+
+    public Optional<BoardResponse> getBoardInfo(Long id) {
+        Optional<Board> result = boardRepository.findById(id);
+
+        //entity -> dto 변환
+        return result.map(BoardResponse::new);
+    }
+
+    @Transactional
+    public Long updateBoardInfo(Long id, Board request) {
+        Board result = boardRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));
+
+        //변경감지
+        result.update(request);
+
+        return id;
+    }
+
+    @Transactional
+    public Long deleteBoardInfo(Long id) {
+        Board result = boardRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));
+
+        //변경감지
+        result.delete();
+
+        return id;
+    }
+
+
 
 }
--- src/main/java/jpa/springBoard/domain/board/dto/BoardRequest.java
+++ src/main/java/jpa/springBoard/domain/board/dto/BoardRequest.java
@@ -1,13 +1,26 @@
 package jpa.springBoard.domain.board.dto;
 
+import jpa.springBoard.domain.board.Board;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BoardRequest {
-    private Long id;
+
+    @NotBlank(message = "제목은 필수입니다.")
     private String title;
+    @NotBlank(message = "내용은 필수입니다.")
     private String content;
+
+    //Request -> Entity 로 변환 시 사용
+    public Board toEntity() {
+        return Board.builder()
+                .title(title)
+                .content(content)
+                .build();
+    }
 }
