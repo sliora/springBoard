115eee371c427a3236e6b1da8dd31fdf18ae31a6
zerom-H <cupdisin@gmail.com>
2022-09-22 23:03:40 +0900

    Feat: 보드 컨트롤러 추가 보드 컨트롤러 추가 및 리스트 조회 개발

---
+++ src/main/java/jpa/springBoard/domain/board/BoardController.java
@@ -0,0 +1,23 @@
+package jpa.springBoard.domain.board;
+
+import jpa.springBoard.domain.board.BoardService;
+import jpa.springBoard.domain.board.dto.BoardResponse;
+import jpa.springBoard.domain.board.dto.BoardSearchCondition;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/api/v1/board")
+    public ResponseEntity<Page<BoardResponse>> list(BoardSearchCondition condition, Pageable pageable) {
+        return ResponseEntity.ok(boardService.getBoardList(condition, pageable));
+    }
+}
