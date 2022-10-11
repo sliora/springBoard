ffc58d10d568daabca933842bedb775007e63b6f
zerom-H <cupdisin@gmail.com>
2022-10-11 22:51:47 +0900

    Fix: Swagger3 버전 업그레이드

--- src/main/java/jpa/springBoard/global/config/WebMvcConfiguration.java
+++ src/main/java/jpa/springBoard/global/config/WebMvcConfiguration.java
@@ -8,12 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    public CommonsMultipartResolver multipartResolver() {
-        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
-        commonsMultipartResolver.setDefaultEncoding("UTF-8");
-        commonsMultipartResolver.setMaxUploadSizePerFile(5 * 1024 * 1024);
-        return commonsMultipartResolver;
-    }
+//    @Bean
+//    public CommonsMultipartResolver multipartResolver() {
+//        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+//        commonsMultipartResolver.setDefaultEncoding("UTF-8");
+//        commonsMultipartResolver.setMaxUploadSizePerFile(5 * 1024 * 1024);
+//        return commonsMultipartResolver;
+//    }
 
 }
